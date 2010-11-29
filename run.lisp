228ce2ef0c708f7ecaf1a7c5110f20abf7942d20
vieira <vieira@yubo.be>
2010-11-29 20:22:39 +0000

    Define função procura de acordo com o enunciado
    Como consequência passa o critério de selecção
    da posição como função anónima lambda.

--- run.lisp
+++ run.lisp
@@ -1,10 +1,5 @@
 ;;;; Atalho Sudoku
 (compile-file "sudoku.lisp" :output-file "sudoku.fas")
 (load "sudoku.fas")
-(defun resolve (ficheiro)
-  (let ((problema (le-tabuleiro ficheiro)))
-    (escreve-tabuleiro (no-tabuleiro (procura-profundidade 
-                                   (make-no :tabuleiro problema) 
-                                   #'objectivo 
-                                   #'sucessores)))))
-
+(defun resolve (ficheiro estrategia)
+  (escreve-tabuleiro (procura ficheiro estrategia)))
